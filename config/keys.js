346fc51bd93a179fa45c6de55cb3decdc3721157
bhavBains <deepbains26@gmail.com>
2018-11-30 22:32:02 -0800

    mongo db connection established, api is running

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://bhav:teradici2018@ds052819.mlab.com:52819/todo_list_teradici",
+};
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const items = require("./routes/api/items");
 
 const app = express();
 
 // Body parser middleware
 app.use(bodyParser.json());
+
+// Use routes
+app.use('/api/items', items);
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose.connect(db)
+  .then(() => console.log("Mongo db is connected..."))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
 
