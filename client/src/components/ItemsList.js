e57d2e46a1986f4c7f4c0b1a4a9b057bdb2788bb
bhavBains <deepbains26@gmail.com>
2018-12-02 17:58:28 -0800

    update items is done and changes to UI

--- client/src/components/ItemsList.js
+++ client/src/components/ItemsList.js
@@ -21,11 +21,39 @@ class ItemsList extends Component {
           items: response.data
         })
       })
-      
+
   }
+
+  handleValidation = () => {
+    let inputText = this.state.name;
+    let errors = {};
+    let formIsValid = true;
+
+    //Name
+    if (!inputText) {
+      formIsValid = false;
+      errors["name"] = "Cannot be empty";
+    }
+
+    if (typeof inputText !== "undefined" || inputText !== null) {
+      if (!inputText.match(/^\w+( \w+)*$/)) {
+        formIsValid = false;
+        errors["name"] = "Only letters";
+      }
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
   // POST request to add new item
   onSubmit = (e) => {
     e.preventDefault();
+    if (!this.handleValidation()) {
+      alert("Error: Please enter letters for your new task, no spaces in the end please");
+      document.getElementById("item").value = "";
+      return
+    }
     const newItem = {
       name: this.state.name.trim()
     }
@@ -33,13 +61,13 @@ class ItemsList extends Component {
       .then(res => {
         document.getElementById("item").value = "";
         this.setState(state => ({
-          items: [{name:res.data.name}, ...state.items],          
-        }))               
-      })    
+          items: [{ name: res.data.name }, ...state.items],
+        }))
+      })
   }
   // Deleting an item
   deleteItem = (id) => {
-    axios.delete(`http://localhost:5000/api/items/${id}`)
+    axios.delete(`/api/items/${id}`)
       .then(res => {
         this.setState({
           // Returning new state by filter method
@@ -48,18 +76,31 @@ class ItemsList extends Component {
       })
   }
 
-  updateItem = (id) => {
-    axios.put(`http://localhost:5000/api/items/${id}`)
+  allItems = () => {
+    axios.get('/api/items')
+    .then(response => {
+      this.setState({
+        isLoading: true,
+        items: response.data
+      })
+    })
+  }
+
+  updateItem = (id, item) => {
+    console.log(item);
+    let data = item;  
+    axios.put(`http://localhost:5000/api/items/${id}`, data)
       .then(res => {
-        this.setState({ ...this.state})
+        this.allItems();
+        document.getElementsByClassName("complete").checked = true;        
       })
   }
-  
+
   render() {
     const { items, error, isLoading } = this.state;
     if (error) {
       return <div>Error: {error}</div>
-    } else if ( !isLoading) {
+    } else if (!isLoading) {
       return <div>Loading...</div>
     } else {
       return (
@@ -68,43 +109,45 @@ class ItemsList extends Component {
             <Container>
               <NavbarBrand>Todo List ({items.length})</NavbarBrand>
             </Container>
-          </Navbar> 
+          </Navbar>
           <ListGroup>
             <Container>
-            <Form onSubmit={this.onSubmit}>
-              <FormGroup>
-                <Input required
-                  type="text"
-                  name="name"
-                  id="item"
-                  placeholder="What's next on your mind..."
-                  onChange ={(e) => this.setState({ name: e.target.value})}
-                />                 
-              </FormGroup>
-            </Form>            
-            <Container>          
-              {items.map(item => ( 
-                <ListGroupItem key={item._id}>                    
-                  <Input addon type="checkbox"  className={item.completed ? `complete` : `items`} onClick={this.updateItem.bind(this, item._id)}  />                    
-                  <label className="strikethrough" key={item._id}>{item.name}</label>
-                  <Button
-                    style={{ float: 'right'}}
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.deleteItem.bind(this, item._id)}
-                  >
-                    &times;
+              <Form onSubmit={this.onSubmit}>
+                <FormGroup>
+                  <Input required
+                    type="text"
+                    name="name"
+                    id="item"
+                    placeholder="What's next on your mind..."
+                    onChange={(e) => this.setState({ name: e.target.value })}
+                  />
+                </FormGroup>
+              </Form>
+              <Container>
+                {items.map(item => (
+                  <ListGroupItem key={item._id}>
+                    <Input addon type="checkbox" className={item.completed ? `complete` : `items`} onClick={this.updateItem.bind(this, item._id, {name: item.name, completed: !item.completed})} />
+                    <label className="strikethrough"  key={item._id}>{item.name}</label>
+                    <Button
+                      style={{ float: 'right' }}
+                      className="remove-btn"
+                      color="danger"
+                      size="sm"
+                      onClick={this.deleteItem.bind(this, item._id)}
+                    >
+                      &times;
                   </Button>
-                </ListGroupItem>              
-              ))}
-            </Container>  
-            </Container>             
+                  </ListGroupItem>
+                ))}
+              </Container>
+            </Container>
           </ListGroup>
-        </Container>      
+        </Container>
       )
     }
   };
 }
 
 export default ItemsList;
+
+
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const router = express.Router();
+// Handling CORS
+const cors = require('cors');
 
 // Item Model
 const Item = require('../../models/Item');
 
 // @route GET api/items
 // @desc Get all items
-router.get('/', (req, res) => {
+router.get('/', cors(), (req, res) => {
   Item.find().sort({date: -1})
     .then(items => res.json(items))
 });
 
 // @route POST api/items
 // @desc Create new item
-router.post('/', (req, res) => {
+router.post('/', cors(), (req, res) => {
   const newItem = new Item({
     name: req.body.name
   })
@@ -23,19 +25,24 @@ router.post('/', (req, res) => {
 
 // @route Delete api/items/:id
 // @desc Delete an item
-router.delete('/:id', (req, res) => {
+router.delete('/:id', cors(), (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false }));
+    .catch(err => res.json({ success: false }));
 });
 
-router.put('/:id', (req,res) => {
-  const id = req.body;
-  Item.findByIdAndUpdate(id, { completed: !completed}, {new:true}, (err, item) => {
-    if (err) return res.status(500).send(err);
-    return res.status(200).json({success: true});
-  })    
-})
+// @route Put api/items/:id
+// @desc Update an item
+router.put('/:id', cors(), (req,res) => {
+  console.log(req.body);
+  Item.findById(req.params.id)
+    .then(item => {
+      item.name = req.body.name;
+      item.completed = req.body.completed;
+      item.save()
+    })
+    .catch(err => console.log(err));
+});
 
 
 module.exports = router;
