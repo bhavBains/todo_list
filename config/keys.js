665ce08ef52a7c2350f240fddb4c152d2b63f62b
Bhavdeep Singh <31292052+bhavBains@users.noreply.github.com>
2018-12-07 13:27:39 -0800

    updated config keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,9 @@
+// Steps to initiate your database
+// Go to https://mlab.com/ , make an account(it's free)
+// Start a new database, make new user and password
+// Copy the link given for new database, enter usename and password of new user
+
 module.exports = {
-  mongoURI: "mongodb://bhav:teradici2018@ds052819.mlab.com:52819/todo_list_teradici",
+  mongoURI: "mongodb://<your-username>:<your-password>@ds052819.mlab.com:52819/todo_list_teradici",
+  // Or you can start new database link, follow instructions above
 };
